7a75f1ff3265324f009d65fd1e531b29820a5873
Sn0w <clement.ozor@protonmail.com>
2022-07-19 05:15:57 +0200

    [develop] => Fix: zeus can now register to all roles without training

--- src/pages/operations/SingleOperation.js
+++ src/pages/operations/SingleOperation.js
@@ -312,7 +312,7 @@ function SingleOperation() {
                                             <div style={{marginBottom: "10px", color: theme.colors.gray[5], display: 'flex', alignItems: 'center'}}>
                                                 <Badge color={"blue"} mr={10}>{role.shortName}</Badge>
                                                 Disponible
-                                                { !isUserRegistered() ? (updatedUser.trained.includes(role.training) || (role.training === undefined && updatedUser.roles.includes('ADMIN_ROLE')) ?
+                                                { !isUserRegistered() ? (updatedUser.trained.includes(role.training) || updatedUser.roles.includes('ADMIN_ROLE') ?
                                                         <Button ml={10} color={"green"} compact onClick={() => registerPlayer(group, role)}>S'inscrire</Button> :
                                                         <Button ml={10} color={"green"} compact disabled title={"Vous n'avez pas la formation requise"}>S'inscrire</Button>) : ""}
                                             </div>
