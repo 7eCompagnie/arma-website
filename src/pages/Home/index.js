7ea93324d45311ec243fcbabda817f2042d6d36b
Sn0w <clement.ozor@protonmail.com>
2022-07-24 16:15:01 +0200

    [develop] => Add minor modification in Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import {Center, Title} from '@mantine/core';
+import {Center, Container, Title} from '@mantine/core';
 import {useNavigate} from "react-router-dom";
 import {useEffect} from "react";
 import background from '../../assets/images/background.jpg'
@@ -12,10 +12,12 @@ function Home() {
     }, [navigate]);
 
     return (
-        <Center style={{ width: '100%', height: '100vh', background: `url(${background}) center center no-repeat`, backgroundSize: 'cover', display: 'flex', flexDirection: 'column'}}>
-            <Title mb={20} style={{ color: 'white', textShadow: '#000 0 0 20px' }}>Bienvenue sur la 7ème Compagnie</Title>
-            <DiscordLoginButton />
-        </Center>
+        <div style={{ width: '100%', height: '100vh', background: `url(${background}) center center no-repeat`, backgroundSize: 'cover'}}>
+            <Center style={{backgroundColor: "rgba(0, 0, 0, .4)", height: "100vh", display: 'flex', flexDirection: 'column'}}>
+                <h1 style={{ color: 'white', textShadow: '#000 0 0 20px', marginBottom: '2rem', fontSize: "38pt"}}>Bienvenue chez la 7ème Compagnie</h1>
+                <DiscordLoginButton />
+            </Center>
+        </div>
     );
 }
 export default Home;
