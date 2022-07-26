5590f973ad584874d4c11215cd1f7b1552f65ecb
Sn0w <clement.ozor@protonmail.com>
2022-07-26 09:59:31 +0200

    [master] => Update discord redirect link

--- src/pages/Home/DiscordLoginButton.js
+++ src/pages/Home/DiscordLoginButton.js
@@ -16,7 +16,7 @@ function DiscordLoginButton() {
                     popupCenter("https://discord.com/api/oauth2/authorize?client_id=964847392543350835&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdashboard&response_type=code&scope=identify",
                         "Connexion à la 7ème Compagnie via Discord", 500, 700);
                 else
-                    popupCenter("https://discord.com/api/oauth2/authorize?client_id=964847392543350835&redirect_uri=http%3A%2F%2Fapp.arma.la7e.fr%2Fdashboard&response_type=code&scope=identify",
+                    popupCenter("https://discord.com/api/oauth2/authorize?client_id=964847392543350835&redirect_uri=https%3A%2F%2Fapp.arma.la7e.fr%2Fdashboard&response_type=code&scope=identify",
                         "Connexion à la 7ème Compagnie via Discord", 500, 700);
                 console.log(window.opener);
             }
